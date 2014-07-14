2a6cb383a2185d8121e7a7fa51f15698a210e4ad
Justin Speers <speersjustin@fastmail.fm>
2014-07-14 07:12:08 -0700

    create function wordOrder, which returns a list of words (seperated by whitespace) as an array of strings.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var wordOrder = function(text) {
+  return text.split(" ");
+
+};
---
+++ spec/specs.js
@@ -0,0 +1,17 @@
+describe("wordOrder", function() {
+  it("Returns each word in a block of text as an array of strings", function() {
+    wordOrder("hello world").should.eql(['hello', 'world']);
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
