424343df640bbf1d18ce40a482a7f3da43a28520
Austin Steward & Justin Speers <student@epicodus.com>
2014-07-16 10:31:12 -0700

    Create and implemented purchase object

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,22 @@
-// returns # of times a given word appears in text
-var countWord = function(text, word) {  
-  return text.toLowerCase().match(new RegExp(word, 'g')).length;
-};
 
-// returns an array of words in text sorted by # of appearances
-// if # of appearances are equal, words are then sorted by order of appearance
-var wordOrder = function(text) {
-  // word in uniqueWordList[x] appears wordCountList[x] # of times
-  var wordCountList = [];   
-  var uniqueWordList = []; 
-
-  var wordList = text.toLowerCase().split(/\W/);
-
-  wordList.forEach(function(word) {
-    if (uniqueWordList.indexOf(word) === -1) {
-      uniqueWordList.push(word);
-      wordCountList.push(countWord(text, word));
-    }
-  });
-
-  // determine maximum # of times any word appears
-  var maxCount = 0;
-  wordCountList.forEach(function(thisCount) {
-    if (thisCount > maxCount) {
-      maxCount = thisCount;
-    }
-  });
-
-  var sortedWords = [];
-  // find each word that appears maximum # of times, then count down, pushing as we go
-  for (var count = maxCount; count > 0; count--) {
-    for(var index=0; index < uniqueWordList.length; index++) {
-      if (wordCountList[index] === count) {
-        sortedWords.push(uniqueWordList[index]);
-      }
-    }
+var Purchase = {
+  price: 0,
+  description: "",
+  setPrice: function(amount) {
+    this.price = amount;
+  },
+  setDescription: function(desc) {
+    this.description = desc;
+  },
+  getPrice: function() {
+    return this.price;
+  },
+  getDescription: function() {
+    return this.description;
   }
-
-  return sortedWords;
 };
+
+$(document).ready(function() {
+
+});
+
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,15 @@
 
-describe("countWord", function() {
-  it("Returns the number of times a word appears in text", function() {
-    countWord('hello world hello hello hello', 'hello').should.equal(4);
+describe("Purchase.getPrice", function() {
+  it("Returns the price of a single purchase", function() {
+    var purchase = Object.create(Purchase);
+    purchase.setPrice(42);
+    purchase.getPrice().should.equal(42);
   });
-  it("Returns the number of times a word appears in text, case insensitive", function() {
-    countWord('heLLo WORld Hello HELLO hello woRLD', 'world').should.equal(2);
+});
+describe("Purchase.getDescription", function() {
+  it("Returns the description of a single purchase", function() {
+    var purchase = Object.create(Purchase);
+    purchase.setDescription("pizza");
+    purchase.getDescription().should.equal("pizza");
   });
-})
-
-describe("wordOrder", function() {
-  it("Returns an array of words in order of appearance", function() {
-    wordOrder("hello world").should.eql(['hello', 'world']);
-  });
-  it("Returns an array of of words sorted in order of appearance, with no duplicates", function() {
-    wordOrder('hello world hello world').should.eql(['hello', 'world']);;
-  });
-  it("Returns an array of words sorted by A) number of occurences in the given text (then) B) order of appearance", function() {
-    wordOrder('hello world hello world yikes world').should.eql(['world', 'hello', 'yikes']);
-  }); 
 });
