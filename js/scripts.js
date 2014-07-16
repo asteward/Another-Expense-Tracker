176db6792fee2597b52660d0f294826e44067ab2
Justin Speers <speersjustin@fastmail.fm>
2014-07-15 21:46:43 -0700

    wordOrder returns array of words sorted first by the number of times it appears, then by order of appeance

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,16 @@
-var countWord = function(text, word) {
-
-  if (text.indexOf(word) === -1) {
-   
-    return 0;
-  }
-
-  var count = 0;
-  var nextIndex = 0;
- 
-  while (nextIndex !== -1) {
-    text = text.substr(nextIndex+1);
-    nextIndex = text.indexOf(word);
-    count++;
-  } 
-
-  return count;
+// returns # of times a given word appears in text
+var countWord = function(text, word) {  
+  return text.toLowerCase().match(new RegExp(word, 'g')).length;
 };
 
-
-
+// returns an array of words in text sorted by # of appearances
+// if # of appearances are equal, words are then sorted by order of appearance
 var wordOrder = function(text) {
+  // word in uniqueWordList[x] appears wordCountList[x] # of times
+  var wordCountList = [];   
+  var uniqueWordList = []; 
 
-  text = text.toLowerCase();
-  var wordList = text.split(/\W/);
-
-  // index in one corresponds to index of the other
-  var wordCountList = [];   // integers indicating # of times a word occurs
-  var uniqueWordList = []; // unique words
-
-  var sortedWords = [];
+  var wordList = text.toLowerCase().split(/\W/);
 
   wordList.forEach(function(word) {
     if (uniqueWordList.indexOf(word) === -1) {
@@ -36,21 +18,24 @@ var wordOrder = function(text) {
       wordCountList.push(countWord(text, word));
     }
   });
-  
-  // left off here really
-  // uniqueWordList[] contains each unique word
-  // wordCountList[] contains the # of times those words exist
 
-  // var maxCount = 0;
+  // determine maximum # of times any word appears
+  var maxCount = 0;
+  wordCountList.forEach(function(thisCount) {
+    if (thisCount > maxCount) {
+      maxCount = thisCount;
+    }
+  });
 
-  // for(var index = 0; index < uniqueWordList.length; index++) {
-  //   maxCount = wordCountList[index];
+  var sortedWords = [];
+  // find each word that appears maximum # of times, then count down, pushing as we go
+  for (var count = maxCount; count > 0; count--) {
+    for(var index=0; index < uniqueWordList.length; index++) {
+      if (wordCountList[index] === count) {
+        sortedWords.push(uniqueWordList[index]);
+      }
+    }
+  }
 
-  //   for(var index2 = index; index2 < uniqueWordList.length; index++) {
-  //     if 
-  //   }
-  // }
-  
-  sortedWords = uniqueWordList.slice(); // temporary, to return unique word list
   return sortedWords;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,9 @@ describe("countWord", function() {
   it("Returns the number of times a word appears in text", function() {
     countWord('hello world hello hello hello', 'hello').should.equal(4);
   });
+  it("Returns the number of times a word appears in text, case insensitive", function() {
+    countWord('heLLo WORld Hello HELLO hello woRLD', 'world').should.equal(2);
+  });
 })
 
 describe("wordOrder", function() {
@@ -12,4 +15,7 @@ describe("wordOrder", function() {
   it("Returns an array of of words sorted in order of appearance, with no duplicates", function() {
     wordOrder('hello world hello world').should.eql(['hello', 'world']);;
   });
+  it("Returns an array of words sorted by A) number of occurences in the given text (then) B) order of appearance", function() {
+    wordOrder('hello world hello world yikes world').should.eql(['world', 'hello', 'yikes']);
+  }); 
 });
