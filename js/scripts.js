82946f553aa9e65b87e54671a0609a065105e914
Austin Steward & Justin Speers <student@epicodus.com>
2014-07-16 16:39:08 -0700

    Fixed display of floating-point values; modified CSS; added hammer's easter egg

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
+var hammerTime = function() {
+    var img = $("#plane"),
+        width = img.get(0).width,
+        screenWidth = $(window).width(),
+        duration = 5000;
+
+    function animatePlane() {
+        img.css("left", -width)
+           .animate({
+                "left": screenWidth
+            }, duration, animatePlane);
+    };
+
+    animatePlane();
+};
+
 
 var Purchase = {
-  price: 0,
+  price: parseFloat(0),
   description: "",
   setPrice: function(amount) {
-    this.price = amount;
+    this.price = parseFloat(amount).toFixed(2);
   },
   setDescription: function(desc) {
     this.description = desc;
   },
   getPrice: function() {
-    return this.price;
+    return parseFloat(this.price).toFixed(2);
   },
   getDescription: function() {
     return this.description;
@@ -48,6 +64,11 @@ $(document).ready(function() {
     newCategory.initialize($("input#category").val());
     categoryList.push(newCategory);
 
+    if($("input#category").val() === "hammer") {
+      hammerTime();
+      $("#secret").show();
+    }
+
     $("ul#cat-list").empty();
     categoryList.forEach(function(thisCategory) {
       $("ul#cat-list").append("<li>" + thisCategory.getDescription() + "</li>");
@@ -58,10 +79,15 @@ $(document).ready(function() {
         $("#purchase-category").text(activeCategory.getDescription());
         $("#latter-columns").show();
         $("tbody#add-table").empty();
+
+        var purchaseTotal = 0.0;
         activeCategory.getPurchaseList().forEach(function(purchase) {
          $("tbody#add-table").append("<tr><td>" + purchase.getDescription() + "</td>" +
-                                  "<td>" + purchase.getPrice() + "</td></tr>");
+                                  "<td class='moneh'>" + purchase.getPrice() + "</td></tr>");
+         purchaseTotal += parseFloat(purchase.getPrice());
         });
+
+        $("tbody#add-table").append("<tr><td>Total:</td><td class='moneh'>$" + parseFloat(purchaseTotal) + "</td></tr>");
         $("#description").focus();
       });
     });
@@ -85,13 +111,16 @@ $(document).ready(function() {
 
     $("tbody#add-table").empty();
 
+    var purchaseTotal = parseFloat(0);
     activeCategory.getPurchaseList().forEach(function(purchase) {
         $("tbody#add-table").append("<tr><td>" + purchase.getDescription() + "</td>" +
-                                  "<td>" + purchase.getPrice() + "</td></tr>");
+                                  "<td class='moneh'>" + purchase.getPrice() + "</td></tr>");
+        purchaseTotal += parseFloat(purchase.getPrice());
     });
-
+    $("tbody#add-table").append("<tr><td>Total:</td><td class='moneh'>$" + parseFloat(purchaseTotal) + "</td></tr>");
     $("#description").focus();
 
   });
 });
+
 
